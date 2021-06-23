f3a9d3d984315493cb4602e7000e55d5769a247d
Mike Fernando <mikera2020@outlook.com>
2021-06-23 19:03:27 -0300

    :sparles:NLW-aula-02

---
+++ main.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('ul li a')
+
+for (const element of links) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+// MUDAR O HEADER DA PÁGINA QUANDO DER SCROLL
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
